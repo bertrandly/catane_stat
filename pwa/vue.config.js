d3ea3d2b75680c9785a414972168ec196b17535d
bertrand <bertranddilly@yahoo.fr>
2020-02-27 13:04:21 +0100

    gestion des icones

--- pwa/vue.config.js
+++ pwa/vue.config.js
@@ -12,6 +12,13 @@ module.exports = {
     msTileColor: '#fbac36',
     manifestOptions: {
       background_color: '#a02e2d'
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
     }
   }
 }
