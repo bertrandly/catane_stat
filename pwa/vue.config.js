db9a67153f8f51b913819d118e65822721a9c8f2
bertrand <bertranddilly@yahoo.fr>
2020-02-27 12:21:59 +0100

    ajout style webapp

--- pwa/vue.config.js
+++ pwa/vue.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   outputDir: '../public/pwa',
   publicPath: '/pwa',
+
   devServer: {
     proxy: 'http://localhost:8002'
+  },
+
+  pwa: {
+    name: 'Catane',
+    themeColor: '#a02e2d',
+    msTileColor: '#fbac36',
+    manifestOptions: {
+      background_color: '#a02e2d'
+    }
   }
 }
