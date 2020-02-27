4bf00ff7f7b2a89c60a57c5527046eb3b62a8160
bertrand <bertranddilly@yahoo.fr>
2020-02-27 12:21:07 +0100

    ajout filtre date

--- pwa/src/main.js
+++ pwa/src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import moment from 'moment'
 
 // datepicker
 import { Datetime } from 'vue-datetime'
@@ -14,6 +15,13 @@ Vue.use(Datetime)
 Vue.component('datetime', Datetime)
 
 Vue.config.productionTip = false
+
+//creating a filter to display date
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('DD/MM/YYYY hh:mm')
+  }
+})
 
 new Vue({
   router,
